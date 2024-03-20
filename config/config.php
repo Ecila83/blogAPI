7cb2df4de7a2354cc6d3d208ffa2d472f298564c
Alice Marique <mariquealice83@gmail.com>
2024-03-20 22:26:22 +0100

    corection

--- config/config.php
+++ config/config.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-
 function connect_db() {
     $dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'];
     $user = $_ENV['DB_USER'];
