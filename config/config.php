87ccfa0bab910e4f0328a66a37fc4d80791c0bf9
Alice Marique <mariquealice83@gmail.com>
2024-03-20 14:40:09 +0100

    config base de donnée

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+function connect_db() {
+    $dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'];
+    $user = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASS'];
+
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Erreur de connexion à la base de données : " . $e->getMessage());
+    }
+}
